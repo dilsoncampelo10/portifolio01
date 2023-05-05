f475da76bd876de913587727223e36b3c80c2686
dilsoncampelo10 <dilsono316@gmail.com>
2023-05-05 16:09:26 -0300

    nav

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,48 @@
+import * as N from './styles';
+import { FaAlignJustify, FaHome, FaUserAlt, FaHeadphonesAlt, FaGithub, FaLinkedin } from 'react-icons/fa';
+
+export default function Navbar() {
+
+    const openMenu = () => {
+
+    }
+    return (
+        <N.Nav>
+            <div>
+                <N.IconArea>
+                    <FaAlignJustify color='white' />
+                </N.IconArea>
+            </div>
+            {/* <N.ButtonMenu onClick={openMenu}>
+                abrir
+            </N.ButtonMenu> */}
+            <N.ListNav>
+                <N.ListItem>
+                    <N.NavItem href="#">
+                        <FaHome />
+                    </N.NavItem>
+                </N.ListItem>
+                <N.ListItem>
+                    <N.NavItem href="#">
+                        <FaUserAlt />
+                    </N.NavItem>
+                </N.ListItem>
+                <N.ListItem>
+                    <N.NavItem href="#">
+                        <FaHeadphonesAlt />
+                    </N.NavItem>
+                </N.ListItem>
+            </N.ListNav>
+            <N.IconArea>
+                <N.NavItem>
+                    <N.IconArea><FaGithub />  </N.IconArea>
+                </N.NavItem>
+                <N.NavItem>
+                    <N.IconArea><FaLinkedin />  </N.IconArea>
+                </N.NavItem>
+
+
+            </N.IconArea>
+        </N.Nav>
+    )
+}
---
+++ src/components/Navbar/styles.ts
@@ -0,0 +1,42 @@
+import styled, { css } from 'styled-components';
+
+export const IconArea = styled.div`
+    cursor: pointer;
+    margin-bottom: 10px;
+`
+export const Nav = styled.nav`
+    background-color: #24292F;
+    flex-wrap: wrap;
+    flex-direction: column;
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    height: 100vh;
+    width: 5%;
+  
+`
+// export const ButtonMenu = styled.button`
+//     position: absolute;
+//     z-index: 2;
+//     width: 50px;
+//     height: 50px;
+//     border-radius: 50%;
+//     background-color: #fff;
+//     outline: 0;
+//     border: 0;
+// `
+export const ListNav = styled.ul`
+   
+   
+    background-color: #24292F;
+    list-style:none;
+`
+export const ListItem = styled.li`
+    margin-bottom: 20px;
+
+`
+export const NavItem = styled.a`
+   
+    color: #fff;
+    text-decoration: none;
+`
